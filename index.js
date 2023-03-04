8594369884dca1a06b39cee8e30bd193f456dfea
IslamAhmed-93 <eslam.ahmed.hussien94@gmail.com>
2023-03-04 16:54:48 +0200

    Pushing Node-Js App data from docker course to my github repo

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+//init app
+const PORT = 4000;
+const app = express();
+
+app.get('/', (req, res) => res.send('<h1> Hello Trestmerge From inside Ubuntu Desktop LTS 22.04!</h1>'));
+
+app.listen(PORT, () => console.log(`App is up and running on port: ${PORT}`) );
