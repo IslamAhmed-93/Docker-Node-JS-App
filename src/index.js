fdecbc2cc37fe340df220e255a2391cfd52d9b97
IslamAhmed-93 <eslam.ahmed.hussien94@gmail.com>
2023-04-02 14:48:19 +0200

    adding docker compose lecture files & commands

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ const express = require('express');
 const PORT = 4000;
 const app = express();
 
-app.get('/', (req, res) => res.send('<h1> Hello Trestmerge From inside Ubuntu Desktop# Hi App</h1>'));
+app.get('/', (req, res) => res.send('<h1> Hello Trestmerge From Ubuntu Desktop from inside #docker compose container </h1>'));
 
 app.listen(PORT, () => console.log(`App is up and running on port: ${PORT}`) );
