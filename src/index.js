c6c4f88121ac1df41c9a81f154ec4a9b86070aa5
IslamAhmed-93 <eslam.ahmed.hussien94@gmail.com>
2024-09-08 16:22:05 +0300

    pushing latest changes in the repo

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 
 //init app
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 const app = express();
 
-app.get('/', (req, res) => res.send('<h1> Hello Trestmerge From Ubuntu Desktop from inside #docker compose container </h1>'));
+app.get('/', (req, res) => res.send('<h1> Hello Trestmerge From Ubuntu Desktop from inside #docker container</h1>'));
 
 app.listen(PORT, () => console.log(`App is up and running on port: ${PORT}`) );
