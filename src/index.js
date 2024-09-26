3adb4fefbabf4a15aff259eec6208dbe6cf7f4b3
IslamAhmed-93 <eslam.ahmed.hussien94@gmail.com>
2024-09-26 04:09:49 +0300

    pushing nginx load-balancing lecture commands and work to the repo

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const redis = require('redis');
+const os = require('os');
 
 //init app
 const PORT = process.env.PORT || 4000;
 const app = express();
+
+//connect to redis
+const REDIS_PORT = 6379;
+const REDIS_HOST = 'redis';
+const redisClient = redis.createClient({
+    url: `redis://${REDIS_HOST}:${REDIS_PORT}`
+});
+redisClient.on('error', err => console.log('Redis Client Error', err));
+redisClient.on('connect', () => console.log('connected to redis....'));
+redisClient.connect();
 
 // connect db
 const DB_USER = 'root';
@@ -12,8 +24,18 @@ const DB_PORT = 27017;
 const DB_HOST = 'mongo';
 
 const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
-mongoose.connect(URI).then(() => console.log('connected to db.....')).catch((error) => console.log('failed to connect to db: ',error));
+mongoose.connect(URI).then(() => console.log('connect to db.....')).catch((error) => console.log('failed to connect to db: ',error));
 
-app.get('/', (req, res) => res.send('<h1> Hello Trestmerge from inside $docker hub aws</h1>'));
+app.get('/', (req, res) => {
+    redisClient.set('products','products....');
+    console.log(`traffuc from ${os.hostname}`);
+    res.send('<h1> Hello Trestmerge From Ubuntu Desktop from inside $docker container</h1>')
+});
+
+app.get('/data', async (req, res) => {
+    const products =  await redisClient.get('products');
+    console.log(`traffuc from ${os.hostname}`);
+    res.send(`<h1> Hello Trestmerge From Ubuntu Desktop from inside @#docker container</h1> <h2>${products}</h2>`)
+});
 
 app.listen(PORT, () => console.log(`App is up and running on port: ${PORT}`) );
